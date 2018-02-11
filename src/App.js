f6e9c32cad60e511a79936cbbfbd913c13bcd494
Igor Chiriac <igor@puck.eu>
2018-02-11 14:09:19 +0100

    Add material components library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
+import AppBar from "material-ui/AppBar";
+import Toolbar from "material-ui/Toolbar";
+import Typography from "material-ui/Typography";
+import Button from "material-ui/Button";
+import IconButton from "material-ui/IconButton";
+import MenuIcon from "material-ui-icons/Menu";
+import Reboot from "material-ui/Reboot";
 
 class App extends React.Component {
-  render () {
-    return <p> TWL app hello !</p>;
+  render() {
+    const theme = createMuiTheme({});
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Reboot />
+        <div>
+          <AppBar position="static">
+            <Toolbar
+              style={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <div style={{ display: "flex" }}>
+                <IconButton color="inherit" aria-label="Menu">
+                  <MenuIcon />
+                </IconButton>
+                <Typography
+                  variant="title"
+                  color="inherit"
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center"
+                  }}
+                >
+                  TWL App
+                </Typography>
+              </div>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </MuiThemeProvider>
+    );
   }
 }
 
